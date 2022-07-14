e7fc522869352122fa5fdb0c867df4923ee94f1f
Eldemcihat <98899165+Eldemcihat@users.noreply.github.com>
2022-07-14 11:13:45 +0300

    Init

--- jdbcIntro/jdbcintro/src/main/java/com/example/App.java
+++ jdbcIntro/jdbcintro/src/main/java/com/example/App.java
@@ -1,29 +1,63 @@
 package com.example;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
+import java.sql.Statement;
 public class App
-{   
-    static String userName="root";
-    static String password="12345";
-    static String dbUrl="jdbc:mysql://localhost:3306/world";
-    public static void main( String[] args )
+{       
+    public static void main( String[] args ) throws SQLException
     {
         Connection connection=null;
+        DbHelper helper = new DbHelper();
+        PreparedStatement statement=null;
         try {
-            connection = DriverManager.getConnection(dbUrl,userName,password);
-            System.out.println("bağlantı kuruldu");
-        } catch (SQLException e) {            
-            e.printStackTrace();
-        }finally{
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+            connection = helper.getConnection();
+            statement = connection.prepareStatement("insert into city(Name,CountryCode,District,Population) values('Düzce','TUR','Düzce','50000')");
+            int result =  statement.executeUpdate();
+            System.out.println(result);
 
+        }catch(SQLException exception){
+            helper.showErrorMessage(exception);
+        }finally{           
+            statement.close();
+            connection.close();
+
+        }
+    }
+
+    public static void selectDemo() throws SQLException{
+        Connection connection=null;
+        DbHelper helper = new DbHelper();
+        java.sql.Statement statement=null;
+        ResultSet resultSet;
+        try {
+            connection = helper.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT code,name,region,Continent FROM world.country");
+            // while(resultSet.next()){
+            //     System.out.println(resultSet.getString("name"));
+            //     System.out.print("-");
+            //     System.out.println(resultSet.getString("region"));
+            //     System.out.print("-");
+            //     System.out.println(resultSet.getString("code"));
+            //     System.out.println("");
+            // }
+            ArrayList<Country> countries = new ArrayList<>();             
+            
+            while(resultSet.next()){
+                 countries.add(new Country(
+                     resultSet.getString("name"),
+                     resultSet.getString("region"),
+                     resultSet.getString("code")));
+             }System.out.println(countries.size());
+                    
+        }catch(SQLException exception){
+            helper.showErrorMessage(exception);
+        }finally{           
+            connection.close();           
+        }
     }
 }
---
+++ jdbcIntro/jdbcintro/src/main/java/com/example/Country.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Country {
+    private String name;
+    private String region;
+    private String code;
+
+    public Country(String name,String region,String code){
+        this.name=name;
+        this.region=region;
+        this.code=code;
+    }
+
+}
